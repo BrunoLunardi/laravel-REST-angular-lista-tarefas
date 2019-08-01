733e502eaa12c1527dbd964306ee97a1a52e35ff
bruno <nardi273@gmail.com>
2019-08-01 15:56:15 -0300

    Recuperando dados da api com o angular

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,32 @@
     var app = angular.module('tarefas', []);
 
     //criação do controller
+    //$scope -> objeto que se refere ao modelo de aplicativo (contexto de execuções para expressõe)
+    //$http -> serviço do angular facilita comunicação http
+    app.controller('TarefasController', function($scope, $http){
+        //função para carregar os dados da api
+        $scope.loadData = function(){
+            //obtém os dados que serão retornados para esta URL
+                //pega os dados retornados da rota e do controller laravel
+            $http.get('http://localhost:8000/api/tarefas').success(function(data){
+                //armazena os dados obtidos na url
+                $scope.dadostarefas = data;
+            });
+        }
+
+        $scope.loadData();
+    });
+
+})();
+
+
+/*
+(function(){
+    //define o nome do modulo que estamos utilizando
+    //está em resoucers/views/index.php
+    var app = angular.module('tarefas', []);
+
+    //criação do controller
     app.controller('TarefasController', function(){
         this.tarefa = tarefa;
     });
@@ -11,4 +37,4 @@
     var tarefa = {'texto':'Teste','autor':'Eu mesmo','status':'concluido'};
 
 })();
-
+*/
--- resources/views/index.php
+++ resources/views/index.php
@@ -9,20 +9,37 @@
 
 </head>
 
-<!-- aciona controller de app.js (TarefasController) -->
+<!-- aciona controller de public/js/app.js (TarefasController) -->
 <!-- ng-controller liga o controller TarefasController com o alias tarefas-->
 <body class="container" ng-controller="TarefasController as tarefas">
     <div class="page-header">
         <h2>Minha lista de tarefas</h2>
     </div>
     
+    <!-- Tabelas para exibir os dados -->
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Tarefa</th>
+                <th>Autor</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <!-- Loop para apresentar os dadostarefas encontrado em public/js/app.js -->
+        <tr ng-repeat="tarefa in dadostarefas">
+            <td>{{tarefa.texto}}</td>
+            <td>{{tarefa.autor}}</td>
+            <td>{{tarefa.status}}</td>
+        </tr>
+    </table>
+    <!-- 
     <div>
-        <!-- Exibe dados recuperados da controller TarefasController de public/js/app.js -->
+        //Exibe dados recuperados da controller TarefasController de public/js/app.js
         <h3>Tarefa: {{tarefas.tarefa.texto}}</h3>
         <h4>Autor: {{tarefas.tarefa.autor}}</h4>
         <h4>Status: {{tarefas.tarefa.status}}</h4>
     </div>
-
+    -->
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.7/angular.js">
     </script>
     <script src="js/app.js"></script>
